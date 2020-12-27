d8faa91302552299e3326161fe7c54fb3d0a36bc
André Basse <andre.basse@gmail.com>
2020-12-27 18:21:47 +0100

    Updated UUID lib

--- go.mod
+++ go.mod
@@ -1,8 +1,10 @@
-module github.com/alash3al/libsrchx
+module github.com/abasse/libsrchx
+
+go 1.15
 
 require (
-	github.com/alash3al/bbadger v0.0.0-20181109122625-e06c832e75f9
-	github.com/blevesearch/bleve v0.7.0
-	github.com/imdario/mergo v0.3.6
-	github.com/satori/go.uuid v1.2.0
+	github.com/alash3al/bbadger v0.0.0-20191001173659-1d440b2b747b
+	github.com/blevesearch/bleve v1.0.14
+	github.com/google/uuid v1.1.2
+	github.com/imdario/mergo v0.3.11
 )
--- index.go
+++ index.go
@@ -9,8 +9,8 @@ import (
 	"github.com/blevesearch/bleve/search/query"
 
 	"github.com/blevesearch/bleve"
+	"github.com/google/uuid"
 	"github.com/imdario/mergo"
-	"github.com/satori/go.uuid"
 )
 
 // Index - the index wrapper
@@ -51,7 +51,7 @@ func (i *Index) Get(id string) (map[string]interface{}, error) {
 // Put - set/update a document
 func (i *Index) Put(data map[string]interface{}) (document map[string]interface{}, err error) {
 	if _, found := data["id"]; !found || data["id"] == "" {
-		uid, err := uuid.NewV4()
+		uid, err := uuid.NewUUID()
 		if err != nil {
 			return nil, err
 		}
@@ -153,7 +153,7 @@ func (i *Index) Search(q *Query) (*SearchResult, error) {
 
 	for _, doc := range res.Hits {
 		doc.Fields["_score"] = doc.Score
-		doc.Fields["_size"] = doc.Size()
+		//doc.Fields["_size"] = doc.Size()
 		doc.Fields["_offset"] = doc.HitNumber
 
 		docs = append(docs, doc.Fields)
